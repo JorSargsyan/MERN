a784622f6a3e2488fdc83cf6b9ccd469fd793de6
JorSargsyan <jorsargsyan@gmail.com>
2019-07-26 00:41:29 +0400

    Alert Component and redux created

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,33 @@ import Navbar from "./components/layout/Navbar"
 import Landing from "./components/layout/Landing"
 import Login from "./components/auth/Login"
 import Register from "./components/auth/Register"
+import Alert from "./components/layout/Alert"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+//redux
+import store from "./store"
+import { Provider } from "react-redux"
 
 import './App.css';
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <Switch>
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-        </Switch>
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+
+          <section className="container">
+            <Alert />
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </section>
+
+        </Fragment>
+      </Router>
+    </Provider>
   )
 }
 
