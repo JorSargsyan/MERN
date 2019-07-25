62c25d100769083923d085ab1f253b1f58b8231d
JorSargsyan <jorsargsyan28@gmail.com>
2019-07-25 19:38:16 +0400

    Frontend cli inited

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, { Fragment } from 'react';
+import Navbar from "./components/layout/Navbar"
+import Landing from "./components/layout/Landing"
+import Login from "./components/auth/Login"
+import Register from "./components/auth/Register"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+
+import './App.css';
+
+const App = () => {
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <Switch>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+        </Switch>
+      </Fragment>
+    </Router>
+  )
+}
+
+
+export default App;
