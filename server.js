153f67d26c6a1de6542b2dc99ff20734454347b7
Jor <jorsargsyan@gmail.com>
2020-05-26 17:48:58 +0400

    prod build

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const connectDb = require("./config/db")
 const bodyParser= require('body-parser')
-
+const path = require('path');
 
 const app = express();
 
@@ -10,9 +10,9 @@ connectDb();
 //Init Middleware 
 app.use(express.json({extended:false}));
 app.use(bodyParser.urlencoded({extended: true}))
-app.get("/",(req,res)=>{
-    res.send("API running");
-})
+// app.get("/",(req,res)=>{
+//     res.send("API running");
+// })
 
 
 app.use("/api/users",require("./routes/api/users"));
@@ -20,7 +20,16 @@ app.use("/api/posts",require("./routes/api/posts"));
 app.use("/api/auth",require("./routes/api/auth"));
 app.use("/api/profile",require("./routes/api/profile"));
 
+//serve static assets in production
 
+if (process.env.NODE_ENV == 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
